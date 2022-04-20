fd7233f84f061771a72ae889a2bb7705c1a66afa
Evgeny <73166581+eshpilev@users.noreply.github.com>
2022-04-21 01:36:38 +0300

    Добавил ссылку на сайт

--- eHonestGamesTlg/Setting.cs
+++ eHonestGamesTlg/Setting.cs
@@ -6,6 +6,7 @@
 		public static string PhoneNumber { get; private set; } = "+79998889999";
 		public static string FirstName { get; private set; } = "NAME";
 		public static string LastName { get; private set; } = "LASTNAME";
+		public static string URL { get; private set; } = "https://google.ru";
 
 		public static int FODMaxTries { get; private set; } = 5;
 		public static int GNMaxTries { get; private set; } = 6;
