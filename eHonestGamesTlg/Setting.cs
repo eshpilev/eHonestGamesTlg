3ccbe5fa1a33652cb084139e20a0bcbf668778a7
Evgeny <73166581+eshpilev@users.noreply.github.com>
2022-04-21 01:23:51 +0300

    Скрыл личные данные

--- eHonestGamesTlg/Setting.cs
+++ eHonestGamesTlg/Setting.cs
@@ -2,10 +2,10 @@
 {
 	internal static class Setting
 	{
-		public static string Token { get; private set; } = "5177599882:AAGelPLh43znU9NWBOXUqxIzkTsUZRLverc";
-		public static string PhoneNumber { get; private set; } = "+79099871299";
-		public static string FirstName { get; private set; } = "Evgeny";
-		public static string LastName { get; private set; } = "Shpilev";
+		public static string Token { get; private set; } = "TOKEN";
+		public static string PhoneNumber { get; private set; } = "+79998889999";
+		public static string FirstName { get; private set; } = "NAME";
+		public static string LastName { get; private set; } = "LASTNAME";
 
 		public static int FODMaxTries { get; private set; } = 5;
 		public static int GNMaxTries { get; private set; } = 6;
